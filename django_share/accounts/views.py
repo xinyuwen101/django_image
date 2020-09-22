61f1eb60a03f337da7871a4029a72b40a8aec391
Xinyu Wen <me.xinyuwen@gmail.com>
2020-09-21 23:58:46 -0700

    Update

--- django_share/accounts/views.py
+++ django_share/accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib import messages, auth
+# from django.contrib import messages, auth
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
