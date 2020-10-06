ab01824459169c1722929cd556d7d7afbf753ab7
Xinyu Wen <me.xinyuwen@gmail.com>
2020-10-06 09:13:44 -0700

    Update

--- django_gallery/settings.py
+++ django_gallery/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [BASE_DIR, "static"]
+STATICFILES_DIRS = [BASE_DIR, 'static']
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
