ca5138bc0c81aa239113a2fe063431abc0edf677
Xinyu Wen <me.xinyuwen@gmail.com>
2021-02-18 02:37:45 -0800

    Update urls.py

--- django_gallery/urls.py
+++ django_gallery/urls.py
@@ -6,11 +6,12 @@ from django.urls import path, include
 from .settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('images/', include('images.urls')),
-    path('social-auth/', include('social_django.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("accounts.urls")),
+    path("images/", include("images.urls")),
+    path("social-auth/", include("social_django.urls")),
 ]
+
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
