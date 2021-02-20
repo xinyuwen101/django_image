f81c199486748cb165a10c30e9d6a5dfb9bea167
Xinyu Wen <me.xinyuwen@gmail.com>
2021-02-20 11:37:15 -0800

    Update

--- static/image/js/main.js
+++ static/image/js/main.js
@@ -1,5 +1,5 @@
 // Upload an images with showing the file name
-$(".custom-file-input").on("change", function () {
-    let fileName = $(this).val().split("\\").pop();
-    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-});
+$('.custom-file-input').on('change', function () {
+  let fileName = $(this).val().split('\\').pop()
+  $(this).siblings('.custom-file-label').addClass('selected').html(fileName)
+})
